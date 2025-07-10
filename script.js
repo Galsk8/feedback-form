5498a12a8055822d446bc1fb3db34a6874fde7ce
Attila <galatigalska@gmail.com>
2025-07-10 21:21:21 +0200

    Added emoji rating and added thank-you screen

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
+let selectedRating = "";
+
 const form = document.getElementById('feedback-form');
 const response = document.getElementById('response');
+const emojis = document.querySelectorAll(".emoji");
+
+emojis.forEach((emoji) => {
+    emoji.addEventListener("click", () => {
+        emojis.forEach(e => e.classList.remove("selected"));
+        emoji.classList.add("selected");
+        selectedRating = emoji.dataset.value;
+    });
+});
 
 form.addEventListener('submit', function (e) {
     e.preventDefault();
     const name = document.getElementById('name').value;
     const message = document.getElementById('message').value;
 
-    response.textContent = `Thank you, ${name}! Your feedback: "${message}"`;
+    form.style.display = 'none';
+
+    response.innerHTML = `
+    <div class="thank-you">
+    <h2>Thank you for your feedback, ${name}!</h2>
+    </div>
+    `; 
 });
